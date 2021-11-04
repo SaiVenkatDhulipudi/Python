a7db21bc4260bd677583696219d684ecbf6ace92
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-11-04 12:59:28 +0530

    Create FrequencyOfDigits.py

---
+++ RegularExpressions/FrequencyOfDigits.py
@@ -0,0 +1,14 @@
+"""
+input:
+1v88886l256338ar0ekk
+
+output:
+1 1 1 2 0 1 2 0 5 0
+"""
+import re 
+s=(re.sub('[^0-9]','',input()))
+d={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+l=list(s)
+for i in l:
+  d[int(i)]+=1
+print(*d.values())
