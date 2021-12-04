5408913574ef745e99dd77d8b43232e1a5c78e78
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-12-04 19:47:28 +0530

    Create BST.py

---
+++ DSA/BST/BST.py
@@ -0,0 +1,59 @@
+class node:
+  def __init__(self,x):
+    self.data=x
+    self.left=None
+    self.right=None
+
+#creating BST
+def create(root,x):
+  if(root==None):
+    return node(x)
+  elif x>root.data:
+    root.right=create(root.right,x)
+  else:
+    root.left=create(root.left,x)
+  return root 
+
+#inorder 
+def inorder(root):
+  if(root==None):
+    return 
+  else:
+    inorder(root.left)
+    print(root.data,end=' ')
+    inorder(root.right)
+
+#postorder
+def postorder(root):
+  if(root==None):
+    return 
+  else:
+    postorder(root.left)
+    postorder(root.right)
+    print(root.data,end=' ')
+#preorder
+
+def preorder(root):
+  if(root==None):
+    return 
+  else:
+    print(root.data,end=' ')
+    preorder(root.left)
+    preorder(root.right)
+    
+
+if __name__=="__main__":
+  n=int(input())
+  l=list(map(int,input().split()))
+  root=None
+  for i in l:
+    root=create(root,i)
+  print("Inorder:",end='')
+  inorder(root)
+  print()
+  print("Postorder:",end='')
+  postorder(root)
+  print()
+  print("Preorder:",end='')
+  preorder(root)
+  
